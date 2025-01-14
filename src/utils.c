994ad52c32fc77bcc7caac2714ae85ca0e5af2bd
Alaka1996 <bgalakananda@gmail.com>
2025-01-14 11:41:34 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include "utils.h"
 
-void print_data(uint16_t *data, int size) {
+void print_data(const uint16_t *data, int size) {
     for (int i = 0; i < size; i++) {
         printf("Sensor Data[%d]: %d\n", i, data[i]);
     }
