e20b0504c25b1b6fa3582295514987f184a0c3cb
Alaka1996 <bgalakananda@gmail.com>
2025-01-14 11:43:01 +0530

    Update processing.c

--- src/processing.c
+++ src/processing.c
@@ -4,7 +4,7 @@
 #include "processing.h"
 #include "sensor.h"
 
-int calculate_average(const uint16_t *data, int size) {
+int calculate_average(uint16_t *data, int size) {
     int sum = 0;
     for (int i = 0; i < size; i++) {
         sum += data[i];
